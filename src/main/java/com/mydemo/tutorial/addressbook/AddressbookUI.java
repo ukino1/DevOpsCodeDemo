b733885b28bc9decc488e8b8173fa801baafd504
theitern <111192083+theitern@users.noreply.github.com>
2022-11-06 21:51:13 -0600

    Update AddressbookUI.java

--- src/main/java/com/mydemo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/mydemo/tutorial/addressbook/AddressbookUI.java
@@ -44,7 +44,7 @@ public class AddressbookUI extends UI {
     ContactForm contactForm = new ContactForm();
 
     // ContactService is a in-memory mock DAO that mimics
-    // a real-world datasource. Typically implemented for
+    // a real-world datasource. Typically implemented for many instances at theITern
     // example as EJB or Spring Data based service.
     ContactService service = ContactService.createDemoService();
 
